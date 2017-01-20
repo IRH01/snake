5f09456df926dbe3bf24b545eb80e99d2b8b95fe
iritchie <469656844@qq.com>
2017-01-20 09:07:09 +0800

    circle

--- test/mongodb/mongoTest.py
+++ test/mongodb/mongoTest.py
@@ -16,14 +16,16 @@ def getAllCollections():
 
     print "start export"
     os.popen("rm -rf /usr/mongodb/bak/*")
-    for colle in db.collection_names():
-        if colle.startswith('circle') == 1 and str(colle).__len__() > 6:
-            export_sh = "/usr/mongodb/bin/mongoexport -h 192.168.105.10:27016 -d irh -c " + colle + " -o /usr/mongodb/bak/" + colle + ".json"
+    for collection_name in db.collection_names():
+        if collection_name.startswith('circle') == 1 and str(collection_name).__len__() > 6:
+            export_sh = "/usr/mongodb/bin/mongoexport -h 192.168.105.10:27016 -d irh -c " + collection_name + " -o /usr/mongodb/bak/" + collection_name + ".json"
             os.popen(export_sh)
 
-            print colle
+            print collection_name
     print "finished export success"
     print "start import"
+
+    db.drop_collection('circle')
 
     ls_str = os.popen("ls /usr/mongodb/bak/").read()
     files = ls_str.split("\n")
@@ -40,7 +42,9 @@ if __name__ == '__main__':
     conn = pymongo.MongoClient(host='192.168.105.9', port=27016)
 
     StartTime = time.time()
-    getAllCollections()
+    # db.drop_collection('circle')
+    db.circle.drop()
+    # getAllCollections()
     EndTime = time.time()
     print "StartTime : %s" % StartTime
     print "EndTime : %s" % EndTime
