d1caee2630e45140d72e341f762118933a5cb075
root <469656844@qq.com>
2017-03-24 01:02:21 +0800

    rabbitmq hello world

---
+++ test/rabbitmq/hello_world_consumer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import pika
+
+credentials = pika.PlainCredentials("guest", "guest")
+# 建立到代理服务器
+conn_params = pika.ConnectionParameters("192.168.1.11", credentials=credentials)
+conn_broker = pika.BlockingConnection(conn_params)
+# 获得信道
+channel = conn_broker.channel()
+# 声明交换器
+channel.exchange_declare(exchange="hello-exchange", type="direct", passive=False, durable=True, auto_delete=False)
+# 声明队列
+channel.queue_declare("hello-queue")
+# 通过键将队列和交换器绑定起来
+channel.queue_bind(queue="hello-queue", exchange="hello-exchange", routing_key="hola")
+
+
+# 用于处理传入消息的函数
+def msg_consumer(method, body):
+    # 消息确认
+    channel.basic.ask(delivery_tag=method.delivery_tag)
+    # 停止消费并推出
+    if body == "quit":
+        channel.basic_cancel(consumer_tag="hello-consumer")
+        channel.stop_consuming()
+    else:
+        print body
+    return
+
+
+# 订阅消费者
+channel.basic_consume(msg_consumer, queue="hello-queue", consumer_tag="hello-consumer")
+# 开始消费
+channel.start_consuming()
---
+++ test/rabbitmq/hello_world_producer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import pika
+
+credentials = pika.PlainCredentials("guest", "guest")
+# 建立连接
+conn_params = pika.ConnectionParameters("192.168.1.11", credentials=credentials)
+conn_broker = pika.BlockingConnection(conn_params)
+# 获取信道
+channel = conn_broker.channel()
+# 声明交换器
+channel.exchange_declare(exchange="hello-exchange", type="direct", passive=False, durable=True, auto_delete=False)
+# 创建纯文本消息
+# msg = sys.argv[1]
+msg = "你好！"
+msg_props = pika.BasicProperties()
+msg_props.conn_type = "text/plain"
+# 发布消息
+channel.basic_publish(body=msg, exchange="hello-exchange", properties=msg_props, routing_key="hola")
