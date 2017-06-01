b4fa407ac5cf6a47a9af5224f22466cbdba6caec
Iritchie.ren <renh@dtds.com.cn>
2017-06-01 17:35:21 +0800

    tijiao

---
+++ test/machine/KNN.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from array import array
+
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
